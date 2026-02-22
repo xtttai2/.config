19d6e76252c87ae57562549d39a5081dd891efb2
xtttai2 <xtttai2@qq.com>
2026-02-22 20:25:57 +0800

    Remove legacy nvim root

--- nvim/lua/plugins/nvim-colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"catgoose/nvim-colorizer.lua",
-	event = "VeryLazy",
-	config = function()
-		require("colorizer").setup()
-	end,
-}
--- nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'sainnhe/everforest',
-  lazy = false,
-  priority = 1000,
-  config = function()
-  vim.g.everforest_diagnostic_line_highlight = 1
-  vim.cmd [[colorscheme everforest]]
-  end,
-}
